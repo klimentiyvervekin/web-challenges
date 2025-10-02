85ed6dbef751728e85bba1434ad10fd171a1ebe5
Klimentiy Vervekin <klimwerwekin@gmail.com>
2025-10-02 12:44:05 +0200

    js dom challenge 2 done

---
+++ javascript-challenges/js-dom-and-events_simple-calculator/js/index.js
@@ -0,0 +1,76 @@
+console.clear();
+
+let operand1 = 12;
+const operand2 = 4;
+
+// ----- Mathematical Operations -----
+
+// Step 1: Use `document.querySelector` to select each button by its `data-js` attribute.
+
+// --v-- write your code here --v--
+const addButton = document.querySelector('[data-js="add"]');
+const subtractButton = document.querySelector('[data-js="subtract"]');
+const multiplyButton = document.querySelector('[data-js="multiply"]');
+const divideButton = document.querySelector('[data-js="divide"]');
+const exponentButton = document.querySelector('[data-js="exponent"]');
+// --^-- write your code here --^--
+
+/* 
+Step 2: Add event listeners for each mathematical operation:
+
+For each operation (add, subtract, multiply, divide, exponent, and modulo):
+1. Add an event listener to the corresponding button.
+2. Within the event listener, perform the operation using `operand1` and `operand2`.
+3. Store the result in a variable.
+4. Log the result to the console.
+*/
+
+// --v-- write your code here --v--
+addButton.addEventListener("click", () => {});
+subtractButton.addEventListener("click", () => {});
+multiplyButton.addEventListener("click", () => {});
+divideButton.addEventListener("click", () => {});
+exponentButton.addEventListener("click", () => {});
+// --^-- write your code here --^--
+
+// ----- Update the First Operand -----
+
+/*
+In the following section, update the value of `operand1` using the buttons in the "Update the First Operand" section.
+Each button should adjust the value of `operand1` and log the new value to the console.
+
+Hint: To allow `operand1` to be updated, you might need to change its declaration.
+
+Step 1: Select each button for updating `operand1` by its `data-js` attribute.
+Step 2: Add event listeners to update `operand1` based on the button clicked. Log the updated value to the console.
+*/
+
+// --v-- write your code here --v--
+operand1 = 12;
+
+addButton.addEventListener("click", () => {
+  const addResult = operand1 + operand2;
+  console.log(addResult);
+});
+
+subtractButton.addEventListener("click", () => {
+  const subtractResult = operand1 - operand2;
+  console.log(subtractResult);
+});
+
+multiplyButton.addEventListener("click", () => {
+  const multiplyResult = operand1 * operand2;
+  console.log(multiplyResult);
+});
+
+divideButton.addEventListener("click", () => {
+  const divideResult = operand1 / operand2;
+  console.log(divideResult);
+});
+
+exponentButton.addEventListener("click", () => {
+  const exponentResult = operand1 ** operand2;
+  console.log(exponentResult);
+});
+
+// --^-- write your code here --^--
